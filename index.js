b7267c973487da425d90ee76623543a5d7c0dd9e
saif-sudo <mdalisaif4@gmail.com>
2022-10-26 03:42:50 +0600

    course json loaded

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/course-cat.json')
+const course = require('./data/course.json')
 
 app.get('/',(req , res) => {
     res.send('Courses API Running');
 });
 app.get('/courses-cat',(req , res) => {
     res.send(categories);
+});
+
+app.get('/course',(req , res) => {
+    res.send(course);
 });
 
 app.listen(port, () => {
