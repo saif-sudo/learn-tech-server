27d26359f02496bd079ced2625eebae92b7f2a2d
saif-sudo <mdalisaif4@gmail.com>
2022-10-27 20:42:57 +0600

    server work done

--- index.js
+++ index.js
@@ -11,12 +11,31 @@ const course = require('./data/course.json')
 app.get('/',(req , res) => {
     res.send('Courses API Running');
 });
-app.get('/courses-cat',(req , res) => {
+app.get('/courses',(req , res) => {
     res.send(categories);
 });
-
-app.get('/course',(req , res) => {
+app.get('/courses/cat/',(req , res) => {
     res.send(course);
+});
+app.get('/courses/cat/:id',(req , res) => {
+    
+    const id = req.params.id;
+
+    if(id === '06' ){
+        res.send(course);
+    }else{
+        const catCourse = course.filter(c => c.category_id === id);
+        res.send(catCourse);
+    }
+   
+    
+});
+
+app.get('/courses/:id',(req , res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(c => c._id === id);
+    res.send(selectedCourse);
+    
 });
 
 app.listen(port, () => {
